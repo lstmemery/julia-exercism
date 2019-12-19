55737e9b049e567056e00d3ddd67beac0e89605b
Matthew Emery <atriumcomplex@gmail.com>
2019-12-19 10:41:16 -0800

    RNA Transcription

--- rna-transcription/rna-transcription.jl
+++ rna-transcription/rna-transcription.jl
@@ -1,4 +1,12 @@
 function to_rna(dna::AbstractString)
-
+    map(lookup, dna)
 end
 
+function lookup(base::Char)
+    transcript_dict = Dict('G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
+    if occursin(base, "GCTA")
+        transcript_dict[base]
+    else
+        throw(ErrorException("$base must be in 'ACGT'"))
+    end
+end
